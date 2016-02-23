e45facb0a5256da503f0cf4db18e748d260f0e59
William Laney <williamlaney@yahoo.com>
2016-02-23 15:53:30 -0500

    Combined functioning RTC/sd code with accel and temp. Reads sensors, creates file on SD but does not write the data to the SD

--- land_sensor.ino
+++ land_sensor.ino
@@ -1,50 +1,79 @@
-//DO NOT FORGET TO SET DATA/TIME. LINE 32
+//DO NOT FORGET TO SET DATA/TIME
 
+//include libraries
+//=====================================
 #include <Wire.h> 
 #include <SFE_MMA8452Q.h> 
-#include <SD.h>
 #include <SPI.h>
-#include <DS3234.h>
+#include "ds3234.h"
+#include <SD.h>
 
-int tmp102Address = 0x48;
-const int chipSelect = 10; //microSD 
+//initialise SD card
+//=====================================
+Sd2Card card;
+SdVolume volume;
+SdFile root;
 
+const int chipSelect = 10; // chip select pin for SD
+
+#define BUFF_MAX 256
+
+//initialise RTC
+//=====================================
+const int cs = 9; // chip select pin for RTC
+
+uint8_t time[8];
+char recv[BUFF_MAX];
+unsigned int recv_size = 0;
+unsigned long prev, interval = 5000;
+
+//initialis accelormater
+//=====================================
 MMA8452Q accel;
 
+
+//initialis temp sensor
+//=====================================
+int tmp102Address = 0x48;
 
 void setup()
 {
   // Open serial communications and wait for port to open:
-  Serial.begin(9600);
-  accel.init(SCALE_4G, ODR_12);
- //intialize RTC:
-  RTC.configure(11,12,13,9);
-//RTC.setDateTime(int d, int mo, int y, int h, int mi, int s);
-RTC.setDateTime(9,2,16,10,30,00);
+   Serial.begin(9600);
 
- SPI.setDataMode(SPI_MODE0); // switch mode to SD
+ //begin RTC  
+ //=====================================
+ DS3234_init(cs, DS3234_INTCN);
+ memset(recv, 0, BUFF_MAX - 1);
+
+//begin SD card  
+ //===================================== 
+  SPI.setDataMode(SPI_MODE0); // switch mode to SD
   // make sure that the default chip select pin is set to
   // output, even if you don't use it:
   pinMode(10, OUTPUT);
   // intialize card:
  SD.begin(chipSelect);
-  //intialize I2C:
-  Wire.begin();
  
-  //may need to swtich SPI mode here
+ //begin I2c  
+ //=====================================
+  Wire.begin();
+
+ //begin accel  
+ //=====================================
+  accel.init(SCALE_8G, ODR_6);
+ 
+//confrim that the everything is working and there is serial communication
   Serial.print("working");
 
 }
 
 void loop() {
-  
- read_write();
- delay(1000);
-}
 
-//=====================================
-void read_write(void) {
-    // strings for assembling the data to log:
+//Read accelerometer 
+ //=====================================
+ 
+  // strings for assembling the data to log:
   String acx = "";
   String acy = "";
   String acz = "";
@@ -57,29 +86,50 @@ void read_write(void) {
     acy += String(Y_accel);
     float Z_accel = accel.cz;
     acz += String(Z_accel);
-    
-  
-  //read temp sensor in celsius
+
+    //print accel to serial
+    Serial.println(acx);
+    Serial.print("\t");
+    Serial.print(acy);
+    Serial.print("\t");
+    Serial.print(acz);
+    Serial.print("\t");
+
+ //read temp sensor in celsius
+ //=====================================
   Wire.requestFrom(tmp102Address,2); 
   byte MSB = Wire.read();
   byte LSB = Wire.read();
   //it's a 12bit int, using two's compliment for negative
   int TemperatureSum = ((MSB << 8) | LSB) >> 4; 
   float celsius = TemperatureSum*0.0625;
-  
-   
-   //read RTC:
-  //SPI.setDataMode(SPI_MODE3);  // switch mode to clock
-  String time = RTC.readDateTime();
-   
-  // open the file. note that only one file can be open at a time,
-  // so you have to close this one before opening another.
-  //SPI.setDataMode(SPI_MODE0);  // switch mode to SD
+  Serial.print(celsius); Serial.print("\t");//print temp to serial
+
+//Read RTC  
+ //=====================================
+   SPI.setDataMode(SPI_MODE1); //switch to RTC mode
+  char buff[BUFF_MAX];
+    unsigned long now = millis();
+    int in;
+struct ts t;
+    // show time
+    DS3234_get(cs, &t);
+        snprintf(buff, BUFF_MAX, "%d.%02d.%02d %02d:%02d:%02d", t.year,
+             t.mon, t.mday, t.hour, t.min, t.sec);
+        Serial.print(buff); //print time to serial
+
+
+ //Write data to SD
+ //=====================================       
+ SPI.setDataMode(SPI_MODE0);  // switch mode to SD
   File dataFile = SD.open("data.txt", FILE_WRITE);
 
   // if the file is available, write to it:
   if (dataFile) {
-    dataFile.print(time);
+    //data writen as tab seprated values in order of
+    //time,  accel in x,  accel in y,  accel in z,  temp
+    dataFile.print("working"); //test writing to SD
+    dataFile.print(buff);
     dataFile.print("\t");
     dataFile.print(acx);
     dataFile.print("\t");
@@ -89,12 +139,11 @@ void read_write(void) {
     dataFile.print("\t");
     dataFile.print(celsius);
     dataFile.print("\t");
-    dataFile.println("\t");
-    dataFile.close();
-     // SPI.setDataMode(SPI_MODE3);  // switch mode to clock
-   
+    dataFile.close(); //need to close the data file or can not wirte new data
+   Serial.println("done"); //confirm data has been writen
   }
-  Serial.print("done");
-  //SPI.endTransaction();
-}
 
+//wait 100ms before begingin the loop again
+delay(100);
+ 
+}
