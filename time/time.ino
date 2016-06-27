5d72625a76a0ece7a39df4753f147b0f596ff395
Ben <bdpowell@email.wm.edu>
2016-06-27 15:17:59 -0400

    Finally fixed the time code

--- time/time.ino
+++ time/time.ino
@@ -1,72 +1,52 @@
-#include <SPI.h>
 #include "ds3234.h"
 
-// something for ds3234 compatibility
-using uint_8 = unsigned char;
-const int cs = 9; // chip select pin
+const int pin = 9;
+inline int prompt(const char *msg);
 
-/*
- * This is a utility for displaying and changing the RTC's current time/date.
- * Data can be entered manually, but for optimal results, wait for it to ask for input
- * and then run autotime.py.
- * 
- * The program will display the current time/date and then ask for a new one with a certain format.
- * If you supply that data, it will update the clock (enter a blank line if you don't want this) and
- * then display the new time.
- * 
- * It will then display the time every second.
- */
-
-// Print the current time, ask to get the new time, then update the clock and print
-// the updated time
-void setup()
-{
-    // Initialization
-    Serial.begin(9600);
-    while (!Serial)
-      ;
-    
-    DS3234_init(cs, DS3234_INTCN);
-    SPI.setDataMode(SPI_MODE1); // we don't use the SD card, just the RTC
-    Serial.println("Enter time as hh:mm:ss HH/DD/YYYY (include spaces, use zeroes):");
-}
-
-// Print the time once every second
-void loop()
-{
-  static bool is_reading = true; // this is only initialized ocne
-  if (is_reading) {
-    while (Serial.available() > 0) {
-      String input = Serial.readString();
-      if (input.length() >= strlen("00:00:00 00/00/0000")) {
-        set_time(input.c_str());
-        is_reading = false;
-      }
-    }
+void setup() {
+  Serial.begin(9600);
+  DS3234_init(pin, DS3234_INTCN);
+  
+  Serial.println(F("Set the time? (y/n)"));
+  while (!Serial.available())
+    ;
+  String s = Serial.readString();
+  if (s.charAt(0) == 'y') {
+    // Write data to the clock
+    ts t;
+    t.year = prompt("Year:");
+    t.mon  = prompt("Month:");
+    t.mday = prompt("Day of the month:");
+    t.wday = prompt("Day of the week: ");
+    t.hour = prompt("Hour: ");
+    t.min  = prompt("Minute: ");
+    t.sec  = prompt("Second: ");
+    DS3234_set(pin, t);
   } else {
-    print_time();
-    delay(1000);
+    Serial.println(F("'Kay..."));
   }
 }
 
-// Set time from a format string.
-void set_time(const char *s) {
+void loop() {
+  // Read data from the clock
   ts t;
-  DS3234_get(cs, &t);
-  sscanf(s, "%hhd:%hhd:%hhd %hhd/%hhd/%d", &(t.hour), &(t.min), &(t.sec),
-            &(t.mon), &(t.mday), &(t.year));
-  
-  DS3234_set(cs, t);
+  DS3234_get(pin, &t);
+  Serial.print(t.mon);   Serial.print('-');
+  Serial.print(t.mday);  Serial.print('-');
+  Serial.print(t.year);
+  Serial.print(' ');
+  Serial.print(t.hour); Serial.print(':');
+  Serial.print(t.min);  Serial.print(':');
+  Serial.println(t.sec);
+  delay(1000);
 }
 
-// Print the current time from the RTC
-void print_time() {
-  // should only need 20 bytes, but we can go a little over
-  const int buff_max = 24;
-  ts t;
-  DS3234_get(cs, &t);
-  char buff[buff_max];
-  snprintf(buff, buff_max, "%02d:%02d:%02d %02d/%02d/%d", t.hour,
-            t.min, t.sec, t.mon, t.mday, t.year);
-  Serial.println(buff);
+inline int prompt(const char *msg) {
+  int i;
+  Serial.println(msg);
+  while (!Serial.available())
+    ;
+  i = Serial.parseInt();
+  Serial.println(i);
+  return i;
 }
