bfe79ef2f5128f4f243a840f627a5a086681db8e
Ben <bdpowell@email.wm.edu>
2016-06-29 15:51:55 -0400

    Modified read_gyro to use the lighter library

--- read_gyro/read_gyro.ino
+++ read_gyro/read_gyro.ino
@@ -1,18 +1,29 @@
-#include <Adafruit_L3GD20.h>
+#include <Light_L3GD20.h>
+#include <Narcoleptic.h>
 
-Adafruit_L3GD20 gyro;
+const byte blinkPin = 5;
+auto pinState = LOW;
 
 void setup() {
   Serial.begin(9600);
-  gyro.begin(gyro.L3DS20_RANGE_250DPS);
+  Gyro::begin();
+
+  pinMode(blinkPin, OUTPUT);
 }
 
 void loop() {
-  gyro.read();
-  Serial.print(gyro.data.x);
+  Gyro::l3gd20Data_t d;
+  Gyro::read(&d);
+  Serial.print(d.x);
   Serial.print('\t');
-  Serial.print(gyro.data.y);
+  Serial.print(d.y);
   Serial.print('\t');
-  Serial.println(gyro.data.z);
-  delay(70);
+  Serial.println(d.z);
+
+  pinState = (pinState == LOW) ? HIGH : LOW;
+  digitalWrite(blinkPin, pinState);
+
+  Serial.end();
+  Narcoleptic.delay(70);
+  Serial.begin(9600);
 }
