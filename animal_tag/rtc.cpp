39d48c9dc39879d4e468f1f7bfa0a5937257fd98
Ben Powell <benjamindavidpowell@gmail.com>
2017-01-13 15:03:32 -0500

    Integrated timer into main code, but in a somewhat hacky manner

--- animal_tag/rtc.cpp
+++ animal_tag/rtc.cpp
@@ -1,38 +1,51 @@
-#include "rtc.hpp"
-#include "ds3234.h"
-#include "debug.h"
 #include <Arduino.h>
 #include <SD.h>
+#include "rtc.hpp"
+#include "debug.h"
+#include "DSRTCLib.h"
 
-constexpr int rtc_cs = 9;
+// Copied from ds3234.h
+// TODO Try using #include, see if it bloats the code
+// TODO Replace with smaller ts instead of attempting backwards compatibility
+struct ts {
+    uint8_t sec;         /* seconds */
+    uint8_t min;         /* minutes */
+    uint8_t hour;        /* hours */
+    uint8_t mday;        /* day of the month */
+    uint8_t mon;         /* month */
+    int year;            /* year */
+    uint8_t wday;        /* day of the week */
+    uint8_t yday;        /* day in the year */
+    uint8_t isdst;       /* daylight saving time */
+    uint8_t year_s;      /* year in short notation*/
+};
 
-inline void print_02d(File,int);
+constexpr byte int_pin = 2; // Interrupt pin (2 or 3)
+constexpr byte int_num = 0; // Interrupt number (???)
 
-// Time data is on the heap to avoid overuse
-// (does this actually help?)
-ts *tme = NULL;
+DSRTCLib rtc(int_pin, int_num);
 
 void rtc_setup() {
-  DS3234_init(rtc_cs, DS3234_INTCN);
+	rtc.start();
 }
 
 void rtc_update() {
   DBGLN("Read RTC");
-  if (!tme)
-    tme = new ts;
-  DS3234_get(rtc_cs, tme);
+  rtc.readTime();
 }
 
 /*
  * Write raw ts to the SD card
  */
 void rtc_write(File sd) {
-  if (!tme) {
-    DBGSTR("Time not set; couldn't write\n");
-    return;
-  }
   DBGLN("Wrote time");
-  sd.write((byte *) tme, sizeof(ts));
-  delete tme;
-  tme = NULL;
+  ts t;
+  t.sec  = rtc.getSeconds();
+  t.min  = rtc.getMinutes();
+  t.hour = rtc.getHours();
+  t.mday = rtc.getDays();
+  t.mon  = rtc.getMonths();
+  t.year = rtc.getYears();
+  // other fields are unused, but included in the parser for some reason
+  sd.write((byte *) &t, sizeof(t));
 }
