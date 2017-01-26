9e9c910f418d43e9534d0a787493c1a169a7e574
Ben Powell <benjamindavidpowell@gmail.com>
2017-01-26 18:09:23 -0500

    Modified profiling sketch for 8MHz Arduino

--- miscellaneous sketches/profiling/profiling.h
+++ miscellaneous sketches/profiling/profiling.h
@@ -10,9 +10,9 @@
 #define mydelay(v) do{PF(MAXPROF-1);delay(v);}while(0)
 
 #if PROFILING_MAIN
-    #define EXTERN
+  #define EXTERN
 #else
-    #define EXTERN extern
+  #define EXTERN extern
 #endif
 
 EXTERN volatile unsigned int prof_array[MAXPROF];
@@ -25,22 +25,22 @@ EXTERN volatile unsigned char prof_has_dumped;
 #define _DLn(x) Serial.println(x)
 
 void clear_profiling_data (void) {
-unsigned char c;
-    for(c = 0 ; c < MAXPROF ; c++) {
-	prof_array[c] = 0;
-    }
+  unsigned char c;
+  for (c = 0 ; c < MAXPROF ; c++) {
+    prof_array[c] = 0;
+  }
 }
 
 void dump_profiling_data(void) {
-unsigned char c;
-    prof_has_dumped = 1;
-    PF(0);
-    for(c = 0 ; c < MAXPROF ; c++) {
-	_DL((int)c);
-	_DL(" ");
-	_DLn((unsigned int)prof_array[c]);
-	prof_array[c] = 0;
-    }
+  unsigned char c;
+  prof_has_dumped = 1;
+  PF(0);
+  for (c = 0 ; c < MAXPROF ; c++) {
+    _DL((int)c);
+    _DL(" ");
+    _DLn((unsigned int)prof_array[c]);
+    prof_array[c] = 0;
+  }
 }
 #endif
 
